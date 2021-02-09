90ebf1d53922803ebe842fefdd2473e7e33da480
khalilholberton <1071@holbertonschool.com>
2021-02-09 13:19:26 +0100

    fix

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -6,12 +6,3 @@ interface Teacher {
   location: string;
   [other: string]: any;
 }
-const teacher3: Teacher = {
-  firstName: "John",
-  fullTimeEmployee: false,
-  lastName: "Doe",
-  location: "London",
-  contract: false,
-};
-
-console.log(teacher3);
