cb1a6d442a3f5576baaf23875100a9662580a72d
khalilholberton <1071@holbertonschool.com>
2021-02-11 12:35:34 +0000

    fix

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -50,7 +50,7 @@ function executeWork(employee: DirectorInterface | TeacherInterface): string {
 
 type Subjects = "Math" | "History";
 
-function teachClass(todayClass: Subjects): string {
+function teachClass(todayClass:Subjects): string {
   if (todayClass === "Math") {
     return `Teaching Math`;
   } else if (todayClass === "History") {
