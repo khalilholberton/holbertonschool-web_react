9fb8b7550cb40d3ee3eddf8cbcb287849a58393a
khalilholberton <1071@holbertonschool.com>
2021-02-11 13:41:55 +0100

    fix

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -52,9 +52,9 @@ type Subjects = "Math" | "History";
 
 function teachClass(todayClass: Subjects): string {
   if (todayClass === "Math") {
-    return "Teaching Math";
+    return `Teaching Math`;
   } else if (todayClass === "History") {
-    return "Teaching History";
+    return `Teaching History`;
   }
 }
 
